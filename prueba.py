7efd59b406d6fcc8afded54dbbd0f6a57dc37288
Sttiingy <marc.cerrillo@atheneasolutions.com>
2023-05-05 13:25:28 +0200

    compressor

--- prueba.py
+++ prueba.py
@@ -2,69 +2,125 @@ import heapq
 import os
 import time
 
+class Node:
+    prob = 0.0
+    symbol = ""
+    encoding = ""
+    visited = False
+    parent = -1
 
-def build_huffman_tree(freq_dict):
-    heap = []
-    for char, freq in freq_dict.items():
-        heapq.heappush(heap, (freq, char))
-    heap1 = []
-    while len(heap) > 1:
-        freq1, char1 = heapq.heappop(heap)
-        freq2, char2 = heapq.heappop(heap)
-        merged_freq = freq1 + freq2
-        merged_char = None
-        merged_node = (merged_freq, merged_char, (freq1, char1), (freq2, char2))
-        heapq.heappush(heap1, merged_node)
-    return heap1[0]
+class Huffman:
+    tree = None
+    root = None
+    nodes = []
+    probs = {}
+    dictEnder = {}
+
+    def __init__(self, symbols):
+        self.initNodes(symbols)
+        self.buildTree()
+        self.buildDictionary()
+
+    def initNodes(self, probs):
+        for symbol in probs:
+            node = Node()
+            node.symbol = symbol
+            node.prob = probs[symbol]
+            node.visited = False
+            self.nodes.append(node)
+            self.probs[symbol] = probs[symbol]
+
+    def buildTree(self):
+        indexMin1 = self.getNodeWithMinimumProb()
+        indexMin2 = self.getNodeWithMinimumProb()
+
+        while indexMin1 != -1 and indexMin2 != -1:
+            node = Node()
+            node.symbol = "."
+            node.encoding = ""
+            prob1 = self.nodes[indexMin1].prob
+            prob2 = self.nodes[indexMin2].prob
+            node.prob = prob1 + prob2
+            node.visited = False
+            node.parent = -1
+            self.nodes.append(node)
+            self.nodes[indexMin1].parent = len(self.nodes) - 1
+            self.nodes[indexMin2].parent = len(self.nodes) - 1
+
+            if(prob1 >= prob2):
+                self.nodes[indexMin1].encoding = "0"
+                self.nodes[indexMin2].encoding = "1"
+            else:
+                self.nodes[indexMin1].encoding = "1"
+                self.nodes[indexMin2].encoding = "0"
+            indexMin1 = self.getNodeWithMinimumProb()
+            indexMin2 = self.getNodeWithMinimumProb()
+
+    def getNodeWithMinimumProb(self):
+        minProb = 1.0
+        indexMin = -1
+
+        for index in range(0, len(self.nodes)):
+            if(self.nodes[index].prob < minProb and (not self.nodes[index].visited)):
+                minProb = self.nodes[index].prob
+                indexMin = index
+
+        if indexMin != -1:
+            self.nodes[indexMin].visited = True
+        return indexMin
+    
+    def showSymbolEncoding(self, symbol):
+        found = False
+        index = 0
+        encoding = ""
+
+        for i in range(0, len(self.nodes)):
+            if self.nodes[i].symbol == symbol:
+                found = True
+                index = i
+                break
+
+        if found:
+            while index != -1:
+                encoding = "%s%s" % (self.nodes[index].encoding, encoding)
+                index = self.nodes[index].parent
+        else:
+            encoding = "Not found"
+
+        return encoding
+    
+    def buildDictionary(self):
+        for symbol in self.probs:
+            encoding = self.showSymbolEncoding(symbol)
+            self.dictEnder[symbol] = encoding
+
+    def encode(self, plain):
+        encoded = ""
+        for symbol in plain:
+            encoded = "%s%s" % (encoded, self.dictEnder[symbol])
+
+        return encoded
+
+start = time.time()
+input = open("pruebas.txt").read()
+simbolos = ''
+probabilidad = []
+msg = input
+
+for i in input:
+    if i in msg:
+        simbolos += i
+        probabilidad.append(float(float(msg.count(i))/ float(len(msg))))
+        msg = msg.replace(i,'')
+symbols = dict(zip(simbolos, probabilidad))
+
+print(len(input))
 
 
-def build_code_table(node, code='', code_table={}):
-    if len(node) === 4
-        freq, char, left, right = node
-    else: 
-        
-    if char:
-        code_table[char] = code
-    else:
-        build_code_table(left, code + '0', code_table)
-        build_code_table(right, code + '1', code_table)
-    return code_table
+huffman = Huffman(symbols)
+encode = huffman.encode(input)
 
-
-def compress(input_file, output_file):
-    # Read input file and calculate character frequencies
-    with open(input_file, 'r') as f:
-        content = f.read()
-    freq_dict = {}
-    for char in content:
-        freq_dict[char] = freq_dict.get(char, 0) + 1
-
-    # Build Huffman tree and code table
-    root_node = build_huffman_tree(freq_dict)
-    code_table = build_code_table(root_node)
-
-    # Compress file and write to output file
-    with open(output_file, 'wb') as f:
-        bit_string = ''
-        for char in content:
-            bit_string += code_table[char]
-        # Pad bit string with zeros to make its length a multiple of 8
-        padding = 8 - len(bit_string) % 8
-        bit_string += '0' * padding
-        # Convert bit string to bytes and write to file
-        byte_string = bytes([int(bit_string[i:i+8], 2) for i in range(0, len(bit_string), 8)])
-        f.write(byte_string)
-
-    # Calculate bits per symbol and compression time
-    bits_per_symbol = sum(freq_dict[char] * len(code_table[char]) for char in freq_dict) / len(content)
-    start_time = time.time()
-    compress(input_file, output_file)
-    end_time = time.time()
-    compression_time = end_time - start_time
-
-    # Print results
-    print(f'Bits per symbol: {bits_per_symbol:.2f}')
-    print(f'Compression time: {compression_time:.2f} seconds')
-
-input = "quijote_clean.txt"
-compress(input, "output")
+with open('code.txt', 'w') as f:
+    f.write(encode)
+end = time.time()
+print("Temps compressió: ", end - start)
